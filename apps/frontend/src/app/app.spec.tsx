9f2a55185b6cc637c8b5ad97e661fb40fef35a30
simonc56 <simchat@gmail.com>
2025-01-31 12:33:07 +0100

    add store provider in app.spec.tsx

--- apps/frontend/src/app/app.spec.tsx
+++ apps/frontend/src/app/app.spec.tsx
@@ -1,23 +1,29 @@
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './store';
 
 import App from './app';
 
 describe('App', () => {
   it('should render successfully', () => {
     const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     );
     expect(baseElement).toBeTruthy();
   });
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     );
     expect(getByText(/Accueil/gi)).toBeTruthy();
   });
