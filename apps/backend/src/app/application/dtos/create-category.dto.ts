0307e9f5c64cb6acc594b7c1c93c6f6f977187bb
simonc56 <simchat@gmail.com>
2025-01-31 19:00:44 +0100

    name property is mandatory in CreateCategoryDto

--- apps/backend/src/app/application/dtos/create-category.dto.ts
+++ apps/backend/src/app/application/dtos/create-category.dto.ts
@@ -4,5 +4,5 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateCategoryDto implements ICreateCategoryDto {
   @IsString()
   @IsNotEmpty()
-  name: string;
+  name!: string;
 }
