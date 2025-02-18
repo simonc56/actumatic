502764ff09d510ad2789726a64baef1622f2cb59
simonc56 <simchat@gmail.com>
2025-02-18 14:25:06 +0100

    refactor: remove unused navigation links and routes from App component

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,4 +1,3 @@
-import { Link, Route, Routes } from 'react-router-dom';
 import Header from 'src/components/Header/Header';
 import SortedNewsList from 'src/components/SortedNewsList/SortedNewsList';
 
@@ -7,37 +6,6 @@ export function App() {
     <div>
       <Header />
       <SortedNewsList />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
     </div>
   );
 }
