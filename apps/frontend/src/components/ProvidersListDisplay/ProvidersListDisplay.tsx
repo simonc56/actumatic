f0a1738b2e5660e7d8ba30223936082193763962
simonc56 <simchat@gmail.com>
2025-02-21 16:49:21 +0100

    feat: 2 columns in ProvidersListDisplay based on total height calcul

--- apps/frontend/src/components/ProvidersListDisplay/ProvidersListDisplay.tsx
+++ apps/frontend/src/components/ProvidersListDisplay/ProvidersListDisplay.tsx
@@ -1,5 +1,6 @@
-import { Title } from '@mantine/core';
+import { Flex, Title } from '@mantine/core';
 import { INewsDto } from '@shared-libs';
+import { useEffect, useState } from 'react';
 import NewsListDisplay from '../NewsListDisplay/NewsListDisplay';
 import classes from './ProvidersListDisplay.module.css';
 
@@ -12,23 +13,65 @@ type ProvidersListProps = {
   }[];
 };
 
+function countProvidersAndNews(
+  newsByProviders: ProvidersListProps['newsByProviders'],
+) {
+  let nbOfProviders = 0;
+  let nbOfNews = 0;
+  for (const provider of newsByProviders) {
+    nbOfProviders++;
+    nbOfNews += provider.news.length;
+  }
+  return [nbOfProviders, nbOfNews];
+}
+
 function ProvidersListDisplay({
   categoryName,
   newsByProviders,
 }: ProvidersListProps) {
+  const [isNarrowScreen, setIsNarrowScreen] = useState(false);
+  const [nbOfProviders, nbOfNews] = countProvidersAndNews(newsByProviders);
+  const totalHeightInPixels = nbOfProviders * 62 + nbOfNews * 28;
+
+  useEffect(() => {
+    // window.matchMedia is not dynamic, so we need to add an event listener
+    // to update the 'isNarrowScreen' state when the screen size changes
+    const mediaWatcher = window.matchMedia('(min-width: 1100px)');
+    setIsNarrowScreen(mediaWatcher.matches);
+
+    function updateIsNarrowScreen(event: MediaQueryListEvent) {
+      setIsNarrowScreen(event.matches);
+    }
+
+    mediaWatcher.addEventListener('change', updateIsNarrowScreen);
+    return function cleanup() {
+      mediaWatcher.removeEventListener('change', updateIsNarrowScreen);
+    };
+  });
+
   if (!newsByProviders?.length) return null;
   return (
     <>
       <Title order={2} className={classes.categoryTitle}>
         {categoryName}
       </Title>
-      {newsByProviders.map((provider) => (
-        <NewsListDisplay
-          providerName={provider.name}
-          key={provider.id}
-          news={provider.news}
-        />
-      ))}
+      <Flex
+        direction="column"
+        gap="xs"
+        wrap="wrap"
+        justify="flex-start"
+        style={
+          isNarrowScreen ? { maxHeight: 500 + totalHeightInPixels / 2 } : {}
+        }
+      >
+        {newsByProviders.map((provider) => (
+          <NewsListDisplay
+            providerName={provider.name}
+            key={provider.id}
+            news={provider.news}
+          />
+        ))}
+      </Flex>
     </>
   );
 }
