94875641c44fac6de912554d93779f0d2b3c3b80
simonc56 <simchat@gmail.com>
2025-02-21 15:20:19 +0100

    feat: add ProviderNewsPage component to display news by provider

---
+++ apps/frontend/src/components/ProviderNewsPage/ProviderNewsPage.tsx
@@ -0,0 +1,24 @@
+import { useParams } from 'react-router-dom';
+import useFetchSortedNews from 'src/hooks/useFetchSortedNews';
+import NewsListDisplay from '../NewsListDisplay/NewsListDisplay';
+
+function ProviderNewsPage() {
+  const sortedNews = useFetchSortedNews();
+  const { id: providerId = '' } = useParams<{ id: string }>();
+  const newsByProviders = sortedNews
+    ?.map((category) => category.providers)
+    .flat();
+  const providerName =
+    newsByProviders?.find((provider) => provider.id === providerId)?.name || '';
+  const news = newsByProviders?.find(
+    (provider) => provider.id === providerId,
+  )?.news;
+  if (!news?.length) return null;
+  return (
+    <div>
+      <NewsListDisplay providerName={providerName} news={news} />
+    </div>
+  );
+}
+
+export default ProviderNewsPage;
