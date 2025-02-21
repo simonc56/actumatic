a761e494b09958c60b2a4c331ada1e2ca2227ddd
simonc56 <simchat@gmail.com>
2025-02-21 15:20:49 +0100

    feat: implement routing for CategoryNewsPage and ProviderNewsPage in the main app

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,11 +1,21 @@
+import { Container } from '@mantine/core';
+import { Route, Routes } from 'react-router-dom';
+import CategoryNewsPage from 'src/components/CategoryNewsPage/CategoryNewsPage';
 import Header from 'src/components/Header/Header';
+import ProviderNewsPage from 'src/components/ProviderNewsPage/ProviderNewsPage';
 import SortedNewsList from 'src/components/SortedNewsList/SortedNewsList';
 
 export function App() {
   return (
     <div>
       <Header />
-      <SortedNewsList />
+      <Container>
+        <Routes>
+          <Route path="/category/:id" element={<CategoryNewsPage />} />
+          <Route path="/provider/:id" element={<ProviderNewsPage />} />
+          <Route path="/" element={<SortedNewsList />} />
+        </Routes>
+      </Container>
     </div>
   );
 }
