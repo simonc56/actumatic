82b489d5b99987e9c783ab8693632439c960d516
simonc56 <simchat@gmail.com>
2025-02-27 13:40:07 +0100

    feat: add greyscale prop to ActumaticLogo for customizable image styling

--- apps/frontend/src/components/Logo/ActumaticLogo.tsx
+++ apps/frontend/src/components/Logo/ActumaticLogo.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo-actumatic-blue.png';
 
-function ActumaticLogo() {
+function ActumaticLogo({ greyscale = false }) {
+  const style = greyscale ? { filter: 'grayscale(1)' } : {};
   return (
     <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
-      <img src={logo} height={28} />
+      <img src={logo} height={28} style={style} />
     </Link>
   );
 }
