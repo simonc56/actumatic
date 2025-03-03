9d25dd0ccd1757aeef6324e033679e308944dc1c
simonc56 <simchat@gmail.com>
2025-03-03 14:12:49 +0100

    feat: enhance logo styling with increased brightness for greyscale option

--- apps/frontend/src/components/Logo/ActumaticLogo.tsx
+++ apps/frontend/src/components/Logo/ActumaticLogo.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/logo-actumatic-blue.png';
 
 function ActumaticLogo({ greyscale = false }) {
-  const style = greyscale ? { filter: 'grayscale(1)' } : {};
+  const style = greyscale ? { filter: 'grayscale(1) brightness(1.5)' } : {};
   return (
     <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
       <img src={logo} height={28} style={style} alt="website logo" />
