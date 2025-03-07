4fa64cbede6262c771d48ac163eb5be13e6e9586
simonc56 <simchat@gmail.com>
2025-03-07 13:49:28 +0100

    use hook to get data in ProviderNewsPage

--- apps/frontend/src/components/ProviderNewsPage/ProviderNewsPage.tsx
+++ apps/frontend/src/components/ProviderNewsPage/ProviderNewsPage.tsx
@@ -4,23 +4,25 @@ import {
   selectCategoryByProviderIdOrSlug,
   selectProviderByIdOrSlug,
 } from 'src/app/store';
-import { useGetAllNewsByProviderQuery } from 'src/features/news';
+import useFetchProviderNews from 'src/hooks/useFetchProviderNews';
 import { getCategoryColor } from 'src/utils/style';
+import FilterBar from '../FilterBar/FilterBar';
 import NewsList from '../NewsList/NewsList';
 
 function ProviderNewsPage() {
   const { slug: providerSlug = '' } = useParams<{ slug: string }>();
+  const date = useAppSelector((state) => state.settings.date);
   const category = useAppSelector(
     selectCategoryByProviderIdOrSlug(providerSlug),
   );
   const provider = useAppSelector(selectProviderByIdOrSlug(providerSlug));
-  const { data: providerNews } = useGetAllNewsByProviderQuery(
-    { providerId: provider?.id },
-    { skip: !provider },
-  );
+  const providerNews = useFetchProviderNews(provider?.id, date);
+
   if (!providerNews?.news?.length || !provider) return null;
   return (
     <div>
+      <FilterBar />
+      <br />
       <NewsList
         providerId={provider.id}
         news={providerNews.news}
