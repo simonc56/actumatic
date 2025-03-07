4629b7f4acebc7e61595e14ec0002120dbf4ec67
simonc56 <simchat@gmail.com>
2025-03-07 15:32:11 +0100

    refactor: simplify useFetchProviderNews and useFetchSortedNews hooks

--- apps/frontend/src/hooks/useFetchProviderNews.ts
+++ apps/frontend/src/hooks/useFetchProviderNews.ts
@@ -1,48 +1,18 @@
 import { useGetAllNewsByProviderQuery } from 'src/features/news';
+import { setQueryOptions } from './useFetchSortedNews';
 
-type NewsQueryOptions = {
+export default function useFetchProviderNews({
+  providerId,
+}: {
   providerId: string | undefined;
-  begin?: string;
-  end?: string;
-  filter?: string;
-  limit?: number;
-};
-
-const setQueryOptions = (
-  providerId: string | undefined,
-  date: string,
-): NewsQueryOptions => {
-  const options: NewsQueryOptions = {
-    providerId,
-    begin: new Date().toISOString().split('T')[0],
-  };
-  if (date === 'yesterday') {
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    options.begin = yesterday.toISOString().split('T')[0];
-    options.end = new Date().toISOString().split('T')[0];
-  }
-  if (date === 'last-week') {
-    const lastWeek = new Date();
-    lastWeek.setDate(lastWeek.getDate() - 7);
-    options.begin = lastWeek.toISOString().split('T')[0];
-  }
-  if (date === 'all-time') {
-    options.begin = undefined;
-  }
-  return options;
-};
-
-export default function useFetchProviderNews(
-  providerId: string | undefined,
-  date: string,
-) {
+}) {
   // const [searchParams] = useSearchParams();
   // const date = searchParams.get('date');
-  const queryOptions = setQueryOptions(providerId, date || 'today');
+  const queryOptions = setQueryOptions(providerId);
   const { data: providerNews } = useGetAllNewsByProviderQuery(queryOptions, {
     refetchOnFocus: true,
     refetchOnReconnect: true,
+    refetchOnMountOrArgChange: true,
   });
 
   return providerNews;
--- apps/frontend/src/hooks/useFetchSortedNews.ts
+++ apps/frontend/src/hooks/useFetchSortedNews.ts
@@ -1,40 +1,47 @@
+import { useAppSelector } from 'src/app/hooks';
 import { useGetSortedNewsQuery } from 'src/features/news';
 
 type NewsQueryOptions = {
+  providerId?: string | undefined;
   begin?: string;
   end?: string;
   filter?: string;
   limit?: number;
 };
 
-const setQueryOptions = (date: string): NewsQueryOptions => {
-  const options: NewsQueryOptions = {
-    begin: new Date().toISOString().split('T')[0],
-  };
+export const setQueryOptions = (
+  providerId: string | undefined,
+): NewsQueryOptions => {
+  const date = useAppSelector((state) => state.settings.date);
+  const options: NewsQueryOptions = {};
+  if (providerId) {
+    options.providerId = providerId;
+  }
+  if (date === 'today') {
+    options.begin = new Date().toISOString().split('T')[0];
+  }
   if (date === 'yesterday') {
     const yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 1);
     options.begin = yesterday.toISOString().split('T')[0];
-    options.end = new Date().toISOString().split('T')[0];
+    options.end = yesterday.toISOString().split('T')[0] + 'T23:59:59';
   }
   if (date === 'last-week') {
     const lastWeek = new Date();
     lastWeek.setDate(lastWeek.getDate() - 7);
     options.begin = lastWeek.toISOString().split('T')[0];
   }
-  if (date === 'all-time') {
-    options.begin = undefined;
-  }
   return options;
 };
 
-export default function useFetchSortedNews(date: string) {
+export default function useFetchSortedNews() {
   // const [searchParams] = useSearchParams();
   // const date = searchParams.get('date');
-  const queryOptions = setQueryOptions(date || 'today');
+  const queryOptions = setQueryOptions(undefined);
   const { data: sortedNews } = useGetSortedNewsQuery(queryOptions, {
     refetchOnFocus: true,
     refetchOnReconnect: true,
+    refetchOnMountOrArgChange: true,
   });
 
   return sortedNews;
