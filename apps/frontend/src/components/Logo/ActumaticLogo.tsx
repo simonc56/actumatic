e94bbb70e3bc740a620b3871c0f700b0ed4015a6
simonc56 <simchat@gmail.com>
2025-03-17 17:37:06 +0100

    add robot icon to website logo

--- apps/frontend/src/components/Logo/ActumaticLogo.tsx
+++ apps/frontend/src/components/Logo/ActumaticLogo.tsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo-actumatic-blue.png';
+import logoRobot from '../../assets/logo-robot.webp';
 
-function ActumaticLogo({ greyscale = false }) {
-  const style = greyscale ? { filter: 'grayscale(1) brightness(1.5)' } : {};
+function ActumaticLogo({ greyscale = false, icon = true }) {
+  const style = {filter: 'grayscale(1) brightness(3)', marginLeft: 90};
+  if (greyscale) {
+    style.filter = 'grayscale(1) brightness(1.5)';
+  }
+  if (!icon) {
+    style.marginLeft = 0;
+  }
   return (
-    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+    <Link to="/" style={{ display: 'flex', alignItems: 'center', gap: 8, position: 'relative' }}>
+      {icon && <img src={logoRobot} height={50} style={{position: 'absolute', left: 18, bottom: -13}} alt="website logo" />}
       <img src={logo} height={28} style={style} alt="website logo" />
     </Link>
   );
