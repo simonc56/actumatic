71313acf2d253014642eb470ced9e673a7463daa
simonc56 <simchat@gmail.com>
2026-05-10 17:08:05 +0200

    chore: update vite to version 8

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,20 +1,24 @@
-/// <reference types='vitest' />
+/// <reference types="vitest/config" />
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import type { ConfigEnv } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
 
-export default ({ mode }: { mode: string }) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd(), '');
   const apiUrl = env.VITE_API_BASE_URL || 'http://localhost:3000';
-  return defineConfig({
+  return {
     root: __dirname,
     cacheDir: '../../node_modules/.vite/apps/frontend',
     server: {
       port: 4200,
       host: 'localhost',
       proxy: {
-        '/api': apiUrl,
+        '/api': {
+          target: apiUrl,
+          changeOrigin: true,
+        },
       },
     },
     preview: {
@@ -29,26 +33,12 @@ export default ({ mode }: { mode: string }) => {
         '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
       },
     },
-    // Uncomment this if you are using workers.
-    // worker: {
-    //  plugins: [ nxViteTsPaths() ],
-    // },
     build: {
       outDir: '../../dist/apps/frontend',
       emptyOutDir: true,
       reportCompressedSize: true,
       commonjsOptions: {
         transformMixedEsModules: true,
-      },
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            mantine: ['@mantine/core', '@mantine/hooks'],
-            'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-            'redux-vendor': ['@reduxjs/toolkit', 'react-redux'],
-            icons: ['@tabler/icons-react'],
-          },
-        },
       },
     },
     test: {
@@ -62,18 +52,5 @@ export default ({ mode }: { mode: string }) => {
         provider: 'v8',
       },
     },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          silenceDeprecations: [
-            'legacy-js-api',
-            'import',
-            'mixed-decls',
-            'global-builtin',
-            'color-functions',
-          ],
-        },
-      },
-    },
-  });
-};
+  };
+});
